54cf682a9dd18ea89b9fe8099dfcb15cb4c25038
岡部　智幸 <okabe@f1-net.co.jp>
2020-02-09 22:51:18 +0900

    初期コミット

---
+++ ReLink.Server/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ReLink.Server.Dao;
+using ReLink.Server.Entiy.DbEntity;
+using ReLink.Server.Models;
+
+namespace ReLink.Server.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            DaoD_Message daoD_Message = new DaoD_Message();
+            var list = daoD_Message.GetList(1);
+            return View(list);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
